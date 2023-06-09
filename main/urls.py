6d9f6ebce88015ff1ca21d3b63f51c947057482a
Mario St-Gelais <mario.stg@videotron.ca>
2023-06-09 12:26:58 -0400

    Update urls and navbar

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("home.urls")),
     path("home/", include("home.urls")),
+    path("sites/", include("sites.urls")),
     path("taxonomy/", include("taxonomy.urls")),
 ]
