7859dd24089ff562d8334cafe778ca957a32ae94
Mario St-Gelais <mario.stg@videotron.ca>
2023-06-12 23:37:57 -0400

    add foreignkey to site

--- sites/models.py
+++ sites/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from geographie.models import Municipalite
 
 # Create your models here.
 class Site(models.Model):
     site_name = models.CharField("Nom du site", max_length=75)
     loc_id = models.CharField("Id de localisation", max_length=10)
+    municipalite = models.ForeignKey(Municipalite, on_delete=models.RESTRICT)
