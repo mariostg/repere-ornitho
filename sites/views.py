f248b577aa683389286048fa5a2a4b7042ef129f
Mario St-Gelais <mario.stg@videotron.ca>
2023-06-24 15:06:49 -0400

    add site favoris

--- sites/views.py
+++ sites/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from sites.models import Site
+from sites.models import Site, Municipalite
 
 
 def sites_favoris(request):
     data = Site.objects.all()
-    print(data)
-    return render(request, "sites/favoris.html", context={"data": data})
+    munic = Municipalite.objects.all()
+    return render(request, "sites/favoris.html", context={"data": data, "munic": munic})
