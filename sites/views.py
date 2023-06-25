5b6c5521906eb24c98e95819d326068cfb69b61c
Mario St-Gelais <mario.stg@videotron.ca>
2023-06-24 22:47:58 -0400

    fix looping in site favoris

--- sites/views.py
+++ sites/views.py
@@ -3,6 +3,8 @@ from sites.models import Site, Municipalite
 
 
 def sites_favoris(request):
-    data = Site.objects.all()
-    munic = Municipalite.objects.all()
-    return render(request, "sites/favoris.html", context={"data": data, "munic": munic})
+    grouped = dict()
+
+    for obj in Site.objects.all():
+        grouped.setdefault(obj.municipalite.name, []).append([obj.site_name, obj.loc_id])
+    return render(request, "sites/favoris.html", context={"data": grouped})
