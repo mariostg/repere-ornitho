8c98b5847ba2e3c8017fe451ca5f32c1901af031
Mario St-Gelais <mario.stg@videotron.ca>
2023-06-24 23:00:34 -0400

    move sass into static dir

--- main/settings_dev.py
+++ main/settings_dev.py
@@ -130,7 +130,7 @@ STATICFILES_FINDERS = [
     "sass_processor.finders.CssFinder",
 ]
 ## Django Sass
-SASS_PROCESSOR_ROOT = BASE_DIR / "sass"
+SASS_PROCESSOR_ROOT = BASE_DIR / "static/sass/"
 
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
