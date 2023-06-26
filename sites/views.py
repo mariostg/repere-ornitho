d5d384ad58309c19cf4a5437793f6b196f8e3949
Mario St-Gelais <mario.stg@videotron.ca>
2023-06-26 10:35:42 -0400

    use regroup filter instead of creating a dict from queryset

--- sites/views.py
+++ sites/views.py
@@ -3,8 +3,4 @@ from sites.models import Site, Municipalite
 
 
 def sites_favoris(request):
-    grouped = dict()
-
-    for obj in Site.objects.all():
-        grouped.setdefault(obj.municipalite.name, []).append([obj.site_name, obj.loc_id])
-    return render(request, "sites/favoris.html", context={"data": grouped})
+    return render(request, "sites/favoris.html", context={"sites": Site.objects.all()})
