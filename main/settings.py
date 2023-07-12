a16a417278400023681a8fcfce7f990216a3e659
Mario St-Gelais <mario.stg@videotron.ca>
2023-07-12 12:51:38 -0400

    delete railway stuff

--- main/settings.py
+++ main/settings.py
@@ -152,14 +152,3 @@ import environ
 env = environ.Env()
 environ.Env.read_env()
 API_KEY = env("API_KEY")
-
-# for Railway deployment
-# Update database configuration from $DATABASE_URL.
-import dj_database_url
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES["default"].update(db_from_env)
-
-# Simplified static file serving.
-# https://pypi.org/project/whitenoise/
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
