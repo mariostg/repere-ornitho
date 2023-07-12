5589a6f29f65643db05b16af0eb0025a6b404089
Mario St-Gelais <mario.stg@videotron.ca>
2023-07-12 13:30:02 -0400

    remove some dev extentions and whitenoise

--- main/settings.py
+++ main/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "django_extensions",
+    # "django_extensions",
     "django.forms",
     # "sass_processor",
     # apps
@@ -49,7 +49,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
