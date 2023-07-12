323fc918c6a9af1f427646634b4e0d4b9cc3f9a1
Mario St-Gelais <mario.stg@videotron.ca>
2023-07-12 14:38:24 -0400

    play with static

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -10,3 +13,4 @@ urlpatterns = [
     path("taxonomy/", include("taxonomy.urls")),
     path("report/", include("ebirdreports.urls")),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
